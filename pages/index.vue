cf5adbed6bb1e086de1d74f88f5b42541b6a68fd
Edwin Khoo <edwin9938@gmail.com>
2024-01-27 16:15:24 +0800

    added header and home page layout

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <section>
+      <h1 class="text-5xl font-bold mt-20">Hello, I'm Edwin</h1>
+      <p class="text-base text-gray-900 p-2 italic">
+        Software Engineering Undergraduate
+      </p>
+    </section>
+    <section class="flex flex-col md:flex-row">
+      <div class="md:w-3/4">
+        <h2 class="text-3xl font-bold mt-20">About Me</h2>
+        <p class="text-base text-gray-900 p-2">
+          I'm a software engineering undergraduate at the University of
+          Waterloo. I'm currently looking for a co-op position for the Winter
+          2022 term.
+        </p>
+      </div>
+      <img
+        src="~/assets/images/Laias.png"
+        class="w-1/2 md:max-w-sm p-8 mx-auto"
+      />
+    </section>
+
+    <section>
+      <h2 class="text-3xl font-bold mt-20">Latest Blog Posts</h2>
+    </section>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
