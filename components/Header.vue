8397507a2634aa2c5d70901560b877ebc300603d
Edwin Khoo <edwin9938@gmail.com>
2024-03-18 17:09:26 +0800

    setup blog module

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,11 @@
 <template>
-  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+  <Disclosure as="nav" class="bg-slate-500" v-slot="{ open }">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="relative flex h-16 items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button-->
           <DisclosureButton
-            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-slate-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
           >
             <span class="absolute -inset-0.5" />
             <span class="sr-only">Open main menu</span>
@@ -31,8 +31,8 @@
                 :href="item.href"
                 :class="[
                   item.current
-                    ? 'bg-gray-900 text-white'
-                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    ? 'bg-slate-900 text-white'
+                    : 'text-gray-300 hover:bg-slate-700 hover:text-white',
                   'rounded-md px-3 py-2 text-sm font-medium',
                 ]"
                 :aria-current="item.current ? 'page' : undefined"
@@ -53,7 +53,7 @@
           :href="item.href"
           :class="[
             item.current
-              ? 'bg-gray-900 text-white'
+              ? 'bg-slate-900 text-white'
               : 'text-gray-300 hover:bg-gray-700 hover:text-white',
             'block rounded-md px-3 py-2 text-base font-medium',
           ]"
@@ -77,7 +77,8 @@ import {
 } from "@headlessui/vue";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
 
-const route = { name: "index" };
+const route = useRoute();
+
 const navigation = [
   { name: "Home", href: "/", current: route.name === "index" },
   { name: "Blogs", href: "/blog", current: route.name.includes("blog") },
---
+++ content/blog/hello-world.md
@@ -0,0 +1,69 @@
+---
+title: Hello, World!
+description: This is a blog post.
+date: 2024-03-18
+cover: https://t3.ftcdn.net/jpg/05/78/33/00/360_F_578330006_9pc5smMWxM5qzAxbayYnqk0RwG5oAulg.jpg
+tags:
+  - blog
+---
+
+# Hello World
+
+Headers
+
+# Header 1
+
+## Header 2
+
+### Header 3
+
+Emphasis Text
+_italic_ or _italic_
+**bold** or **bold**
+
+Lists
+
+- Item 1
+- Item 2
+  - Subitem A
+  - Subitem B
+
+1. First item
+2. Second item
+
+Links
+[Link text](https://www.example.com)
+
+Images
+![Alt text](image_url.jpg)
+
+Code Blocks
+
+```python
+print("Hello, world!")
+```
+
+## Horizontal Rule
+
+Blockquotes
+
+> This is a blockquote.
+
+Tables
+| Column 1 | Column 2 |
+|----------|----------|
+| Row 1 | Row 1 |
+| Row 2 | Row 2 |
+
+Equations
+$E = mc^2$
+
+:::callout{title="Hello World Callout"}
+This is a quick tip about markdown
+:::
+
+```
+
+```
+
+![This is an image](https://t3.ftcdn.net/jpg/05/78/33/00/360_F_578330006_9pc5smMWxM5qzAxbayYnqk0RwG5oAulg.jpg)
